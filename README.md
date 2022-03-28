660aa6f2a16fdd4a69054a096d85e53680c727e9
Beth <bethmelmtv@gmail.com>
2022-03-27 20:49:58 -0700

    added render function

--- README.md
+++ README.md
@@ -7,13 +7,48 @@ If you work on more than one feature at a time, you are guaranteed to multiply y
 ## Making a plan
 
 1) **Make a drawing of your app. Simple "wireframes"** 
+
 1) **Look at the drawing and name the HTML elements you'll need to realize your vision**
+i will need a div for defeated goblin status
+a div for hit point status
+a button form for challenge button
+
 1) **Look at the drawing and imagine using the app. What _state_ do you need to track?** 
+hit points 
+defeated goblins 
 1) **For each HTML element ask: Why do I need this? (i.e., "we need div to display the results in")** 
 1) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How" (i.e., `resultsEl.textContent = newResults`)**
+
+defeatedGoblins.textContent = defeatedGoblins 
+
+hitPoints.textContent = hitPoints
+
+the goblins will need to take the info from form and render and append it and then display them below the form
+
 1) **Find all the 'events' (user clicks, form submit, on load etc) in your app. Ask one by one, "What happens when" for each of these events. Does any state change? Does any DOM update?**
+
+-user click submit form
+  - render and append input 
+
+user clicks on a goblin
+  -the user either hit/miss goblin
+  -alert 
+  - the goblin can either hit/miss user
+  -alert 
+  - update hit points of user if nesc 
+
+
+
 1) **Think about how to validate each of your features according to a Definition of Done. (Hint: console.log usually helps here.)**
+
+
+
 1) **Consider what features _depend_ on what other features. Use this dependency logic to figure out what order to complete tasks.**
+
+-defeated goblin # depends on if user hits goblin
+
+
+
 
 Additional considerations:
 - Ask: which of your HTML elements need to be hard coded, and which need to be dynamically generated?
--- app.js
+++ app.js
@@ -1,119 +1,185 @@
-import { renderGoblin } from './render-utlis.js';
-let defeatedNumberEl = document.getElementById('defeated-number');
-let adventurerHitPointsEl = document.getElementById('adventurer-hit-points');
-//grabbing span eleement of defeater number
-//grabing span element of HP
+import { renderGoblin } from './render-utils.js';
+
+
+const defeatedNumberEl = document.querySelector('#defeated-number');
+const hitPointStatusEl = document.querySelector('#hit-point-status');
 const form = document.querySelector('form');
-//grabbing form
-let goblinListEl = document.querySelector('.goblins');
-//grabbing blank div of goblins 
+const goblinListEl = document.querySelector('#goblins') ;
 
-
-// let goblinName = document.getElementById('goblin-name');
-// let challengeButton = document.getElementById('challenge-button');
-// let listofGoblins = document.getElementById('list-of-goblins');
-
-
- // let state 
+//state
 let defeatedGoblinsCount = 0;
-let playerHitPoints = 10;
+let playerHP = 10;
 
-//why do we neeed to have goblins in state? 
-
+//create an object that contains the info of different goblins, not sure why its within an array
 let goblins = [
-    { name: 'terry', hp: 2 },
-    { name: 'bob', hp: 3 },
+    { name: 'Terry', HP: 10 },
+    { name: 'Bob', HP: 5 },
 ];
 
-  // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 
 
 
-//this event listener takes an input and pushes it to an array
-//and assigns it a random hp
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    const data = new FormData(form);
-    //this line holds the info inputted in form 
 
-    //it will now need to get this new data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { renderGoblin } from './render-utlis.js';
+// let defeatedNumberEl = document.getElementById('defeated-number');
+// let adventurerHitPointsEl = document.getElementById('adventurer-hit-points');
+// //grabbing span eleement of defeater number
+// //grabing span element of HP
+// const form = document.querySelector('form');
+// //grabbing form
+// let goblinListEl = document.querySelector('.goblins');
+// //grabbing blank div of goblins 
+
+
+// // let goblinName = document.getElementById('goblin-name');
+// // let challengeButton = document.getElementById('challenge-button');
+// // let listofGoblins = document.getElementById('list-of-goblins');
+
+
+//  // let state 
+// let defeatedGoblinsCount = 0;
+// let playerHitPoints = 10;
+
+// //why do we neeed to have goblins in state? 
+
+// let goblins = [
+//     { name: 'terry', hp: 2 },
+//     { name: 'bob', hp: 3 },
+// ];
+
+//   // set event listeners 
+//   // get user input
+//   // use user input to update state 
+//   // update DOM to reflect the new state
+
+
+
+// //this event listener takes an input and pushes it to an array
+// //and assigns it a random hp
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     const data = new FormData(form);
+//     //this line holds the info inputted in form 
+
+//     //it will now need to get this new data
 
   
-    //   - Make a new goblin object with that user input
-    const newGoblin = {
-        name: data.get('goblin-name'),
-        //Make a new goblin object with that user input
-        //data.get is a function
-        //data.get is referencing input class in html
-      //in the line above, we're referencing the data variable we just made in line 43
-        hp: Math.ceil(Math.random() * 5),
-      // in this line, we're assigning hp to a random hit points 
-    };
+//     //   - Make a new goblin object with that user input
+//     const newGoblin = {
+//         name: data.get('goblin-name'),
+//         //Make a new goblin object with that user input
+//         //data.get is a function
+//         //data.get is referencing input class in html
+//       //in the line above, we're referencing the data variable we just made in line 43
+//         hp: Math.ceil(Math.random() * 5),
+//       // in this line, we're assigning hp to a random hit points 
+//     };
 
-    goblins.push(newGoblin);
-    //pushing new goblin into an array
+//     goblins.push(newGoblin);
+//     //pushing new goblin into an array
 
-    displayGoblins();
+//     displayGoblins();
     
-});
+// });
 
 
-function displayGoblins() {
-  //we're going to clear out the Dom 
-    goblinListEl.textContent = '';
-    //emptying out the dom so that function dont make replicas 
-    //goblinListEl = list of goblins already made 
+// function displayGoblins() {
+//   //we're going to clear out the Dom 
+//     goblinListEl.textContent = '';
+//     //emptying out the dom so that function dont make replicas 
+//     //goblinListEl = list of goblins already made 
 
-    for (let goblin of goblins) {
-        const goblinEl = renderGoblin(goblin);
-      //for each goblin object in goblins array, we will apply the 
-      //render function, which will create a new div for it 
-        if (goblin.hp > 0) {
-            goblinListEl.addEventListener('click', () => {
-            //we use goblinListEl because that's the element were clicking on
-                if (Math.random() < .33) {
-                    goblin.hp --;
-            //we hit the goblin 
-                    alert('you hit' + goblin.name);
-                } else { 
-                    alert('you tried to hit' + goblin.name + 'but missed!');
-                }
-                if (Math.random() > .33) {
-              //if random # < .33, then you got hit, decrease your HP points
-                    alert(goblin.name + 'hit you!');
-                    playerHitPoints--;
-                } else {
-                    alert(goblin.name + 'tried to hit you but missed!');
-                }
+//     for (let goblin of goblins) {
+//         const goblinEl = renderGoblin(goblin);
+//       //for each goblin object in goblins array, we will apply the 
+//       //render function, which will create a new div for it 
+//         if (goblin.hp > 0) {
+//             goblinListEl.addEventListener('click', () => {
+//             //we use goblinListEl because that's the element were clicking on
+//                 if (Math.random() < .33) {
+//                     goblin.hp --;
+//             //we hit the goblin 
+//                     alert('you hit' + goblin.name);
+//                 } else { 
+//                     alert('you tried to hit' + goblin.name + 'but missed!');
+//                 }
+//                 if (Math.random() > .33) {
+//               //if random # < .33, then you got hit, decrease your HP points
+//                     alert(goblin.name + 'hit you!');
+//                     playerHitPoints--;
+//                 } else {
+//                     alert(goblin.name + 'tried to hit you but missed!');
+//                 }
 
-                if (goblin.hp === 0) {
-                    defeatedGoblinsCount++;
-                }
+//                 if (goblin.hp === 0) {
+//                     defeatedGoblinsCount++;
+//                 }
 
-                if (adventurerHitPoints === 0);
-                alert('Game Over!!!');
+//                 if (adventurerHitPoints === 0);
+//                 alert('Game Over!!!');
         
           
-                adventurerHitPointsEl.textContent = playerHitPoints;
-                defeatedNumberEl.textContent = defeatedGoblinsCount;
+//                 adventurerHitPointsEl.textContent = playerHitPoints;
+//                 defeatedNumberEl.textContent = defeatedGoblinsCount;
 
-                displayGoblins();
+//                 displayGoblins();
           
-            });
+//             });
 
-        }
-        goblinListEl.append(goblinEl);
+//         }
+//         goblinListEl.append(goblinEl);
 
-    }
-}
+//     }
+// }
 
 
-displayGoblins();
+// displayGoblins();
 
     
 
@@ -127,19 +193,19 @@ displayGoblins();
           
   
 
-    //takes new object
-    //renders  it 
-    // appends it to list of goblins 
+//     //takes new object
+//     //renders  it 
+//     // appends it to list of goblins 
 
-  //another event listener that says
-  //if i click on a goblin, and i hit it 
-  //my hp goes up
+//   //another event listener that says
+//   //if i click on a goblin, and i hit it 
+//   //my hp goes up
 
-  //if i click on a goblin, and i miss,
-  //my hp goes down by one
+//   //if i click on a goblin, and i miss,
+//   //my hp goes down by one
 
-  //// if a goblin hits me, their hp goes up by one
-  // if a goblin doesnt hit me, their hp goes down by one 
+//   //// if a goblin hits me, their hp goes up by one
+//   // if a goblin doesnt hit me, their hp goes down by one 
 
 
-//update the numbers of hp points of me and goblin 
+// //update the numbers of hp points of me and goblin 
